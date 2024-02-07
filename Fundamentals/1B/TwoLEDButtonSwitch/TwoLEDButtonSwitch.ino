21cef16cc9beea0d0397936fb9c0bea838da4014
thomasismartinez <98428471+thomasismartinez@users.noreply.github.com>
2024-02-07 00:49:48 -0500

    2 led button switch

--- Fundamentals/1B/TwoLEDButtonSwitch/TwoLEDButtonSwitch.ino
+++ Fundamentals/1B/TwoLEDButtonSwitch/TwoLEDButtonSwitch.ino
@@ -1,9 +1,8 @@
 /*
-  2 Button Toggle
+  2 button toggle
 
-  Switches between 2 leds on and off a light emitting diode(LED) connected to digital pins 3 and 4,
+  Turns on and off a light emitting diode(LED) connected to digital pin 3 and 4,
   when pressing a pushbutton attached to pin 2.
-
 */
 
 // constants won't change. They're used here to set pin numbers:
@@ -13,26 +12,32 @@ const int ledPinRed = 4;    // the number of the LED pin
 
 // variables will change:
 int buttonState = 0;  // variable for reading the pushbutton status
-int prevButtonState = 0;
-bool on = false;
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledPinYellow, OUTPUT);
+  pinMode(ledPinRed, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
 
-  if (buttonState == HIGH) { // check if the pushbutton is pressed.
-    if (prevButtonState == LOW) { // if it was previously not pressed
-      on = !on;
-      digitalWrite(ledPin, on); // sace the previous button state
-    }
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPinRed, HIGH);
+    digitalWrite(ledPinYellow, LOW);
+    Serial.print("Red");
+    
+  } else {
+    // turn LED off:
+    digitalWrite(ledPinRed, LOW);
+    digitalWrite(ledPinYellow, HIGH);
+    Serial.print("Yellow");
   }
-
-  prevButtonState = buttonState;
 }
